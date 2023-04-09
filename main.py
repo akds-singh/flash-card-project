10fce8e5fe28f32816ddacf7f68a6002ab3c50f2
akds-singh <akds.singh@google.com>
2023-04-09 17:44:37 +0530

    adding flip functonality in my project

--- main.py
+++ main.py
@@ -4,25 +4,39 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+# ---------------------------------------------flip card func ---------------------------------------#
+def flip_card(eng_word):
+    # canvas.itemconfig(tagOrId=card_back, state='normal')
+    canvas.itemconfig(tagOrId=card_front, state='hidden' )
+    canvas.itemconfig(tagOrId=title_word, text='English', fill='white')
+    canvas.itemconfig(tagOrId=text_word, text=eng_word, fill='white')
+
+    print("card flip")
+
+
 # --------------------------------------------- Get French Words ----------------------------------- #
 
-# Getting the French and English word form .CSV file  and convert onto dict.
+# Getting the French and English word form .CSV file  and convert onto list of dict.-----
 df = pandas.read_csv(filepath_or_buffer="./data/french_words.csv")
-dict_data = df.to_dict(orient='split')
-# print(dict_data)
-# print(dict_data["data"])
-from_dict_data = {item[0]: item[1] for item in dict_data['data']}
+list_data = df.to_dict(orient='records')
 
 
-# func for choice a random word form above dict
+# print(list_data)
+
+# func for choice a random word form above dict------------------
 def random_french_word():
-    french_words_list = [val for val in from_dict_data.keys()]
-    rand_french_word = random.choice(french_words_list)
+    # french_words_list = [val for val in dict_from_list_data.keys()]
+    rand_item = random.choice(list_data)
+    print(rand_item)
+    french_word = rand_item['French']
+    eng_word = rand_item['English']
+    canvas.itemconfig(tagOrId=card_front, state='normal')
+    canvas.itemconfig(tagOrId=title_word, text='French', fill='black')
+    canvas.itemconfig(tagOrId=text_word, text=french_word, fill='black')
 
-    canvas.itemconfig(tagOrId=french_word, text=rand_french_word)
+    after_id = window.after(ms=3000, func=lambda: flip_card(eng_word))
 
-
-# print(from_dict_data)
 
 # ---------------------------------------------- UI Setup ------------------------------------------- #
 
@@ -34,14 +48,14 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Canvas widget -----------------------------------------
 canvas = tk.Canvas(width=800, height=550, bg=BACKGROUND_COLOR, highlightthickness=0)
 
-card_back = tk.PhotoImage(file='./images/card_back.png')
-canvas.create_image(400, 270, image=card_back)
+card_back_img = tk.PhotoImage(file='./images/card_back.png')
+card_back = canvas.create_image(400, 270, image=card_back_img)
 
-card_front = tk.PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 270, image=card_front)
+card_front_img = tk.PhotoImage(file="./images/card_front.png")
+card_front = canvas.create_image(400, 270, image=card_front_img)
 
-canvas.create_text(400, 150, text='French', font=('Arial', 40, 'italic'))
-french_word = canvas.create_text(400, 253, text='trouve', font=('Arial', 60, "bold"))
+title_word = canvas.create_text(400, 150, text='Language', font=('Arial', 40, 'italic'))
+text_word = canvas.create_text(400, 253, text='Word', font=('Arial', 60, "bold"))
 
 canvas.grid(row=0, column=0, columnspan=2)
 
